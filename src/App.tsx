46f6aea577be7ca1eaae34b8edba52e4afb128fe
Oscar Lopez <oscareduardolp6@gmail.com>
2022-12-15 13:04:42 -0600

    feat(GetNumberOfWords): :art: Initial UI Design
    Initial UI design, works but require refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Center,
+  Heading,
+  Input,
+} from '@chakra-ui/react'
 
 function App() {
-
   return (
-    <></>
+    <>
+      <Center marginTop='3em'>
+        <Card>
+          <CardHeader>
+            <Heading>
+              Selecciona la cantidad de palabras que desea recordar
+            </Heading>
+          </CardHeader>
+          <CardBody>
+            <Input
+              size='lg'
+              placeholder='ejem. 20'
+              type='number'
+              variant='filled'
+              min={1}
+              max={100}
+              textAlign='center'
+            />
+          </CardBody>
+        </Card>
+      </Center>
+    </>
   )
 }
 
