9e1cc7bc9768e169c8d7fd0b7ea3fcc25f21ff13
Oscar Lopez <oscareduardolp6@gmail.com>
2022-12-28 18:25:13 -0600

    fix(global): :ambulance: Removed issued import
    Removed import of App.css in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Router'
-import './App.css'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
